99ba5c0b6b4e73439f386e81a9104120e1b5e0cc
faithremo <faithmakumi@gmail.com>
2022-04-03 18:25:15 +0300

    creating a form for users to signin

--- miniIG/forms.py
+++ miniIG/forms.py
@@ -5,3 +5,12 @@ from crispy_forms.layout import Submit, Layout, Field
 
 class PostForm(forms.ModelForm):
     helper = FormHelper()
+    helper.form_method = 'POST'
+    helper.add_input(Submit('Post', 'Post', css_class='btn-primary'))
+    
+    class Meta:
+        model = Post
+        fields = [
+            'image',
+            'caption'
+        ]
