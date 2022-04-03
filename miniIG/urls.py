b383c28de183d3d66fbecea0ebd59777e1949ad4
faithremo <faithmakumi@gmail.com>
2022-04-03 18:53:05 +0300

    created post_create.html file

--- miniIG/urls.py
+++ miniIG/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from .views import (
-    PostListView
+    PostListView,
+    PostCreateView,
 )
 
 app_name = 'miniIG'
@@ -8,4 +9,5 @@ app_name = 'miniIG'
 urlpatterns = [
     #local : http://127.0.0.1:8000/
     path('', PostListView.as_view(), name='post_list'),
+    path('new/', PostCreateView.as_view(), name='post_create'),
 ]
